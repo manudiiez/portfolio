d65b793a6a74a88a0a7449179a973ec96a59edc2
manolo2829 <manudiez2003@gmail.com>
2024-02-07 15:55:57 -0300

    about section

--- src/sections/AboutSection.jsx
+++ src/sections/AboutSection.jsx
@@ -11,10 +11,21 @@ const AboutSection = () => {
                 <div className={styles.body}>
                     <div className={styles.text}>
                         <h4>Mi nombre es Manuel Diez de Oñate, soy un Desarrollador Full Stack. Estoy emocionado por asumir nuevos desafíos y seguir creciendo como desarrollador.</h4>
-                        <h4>Mi conjunto de habilidades incluye experiencia en:</h4>
-                        <h4>HTML, CSS, JavaScript, React, Node.js, MongoDB, MySQL, Git y WordPress.</h4>
+                        <div>
+                            <h4>Mi conjunto de habilidades incluye experiencia en:</h4>
+                            <ul>
+                                <li>
+                                    <h4><strong>Front-end:</strong> HTML, CSS, JavaScript, React.js, Next.js, Tailwind, Astro y  Wordpress</h4>
+                                </li>
+                                <li>
+                                    <h4><strong>Back-end:</strong> Node.js, Strapi, PHP, MongoDB y MySQL</h4>
+                                </li>
+                                <li>
+                                    <h4><strong>Otros:</strong> Git, Notion y Figma</h4>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
-                    <div className={styles.image}></div>
                 </div>
             </div>
         </section>
